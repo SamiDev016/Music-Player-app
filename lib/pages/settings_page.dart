ada5dc82c813d9d3e9e54d7e0bb818820572f041
samidev016 <adelim0555@gmail.com>
2024-08-11 13:45:30 +0200

    finished with theme and toggle theme settings

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:music_player_application/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -6,15 +9,48 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
-        title: const Text("S E T T I N G S", style: TextStyle(color: Colors.white),),
+        title: const Text(
+          "S E T T I N G S",
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
         foregroundColor: Colors.white,
         shadowColor: Theme.of(context).colorScheme.inversePrimary,
         elevation: 5,
       ),
-
+      body: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(10)
+        ),
+        padding: const EdgeInsets.all(25),
+        margin: const EdgeInsets.all(25),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              "Dark Mode",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            CupertinoSwitch(
+                value: Provider.of<ThemeProvider>(context, listen: false)
+                    .isDarkMode,
+                onChanged: (value) =>
+                    Provider.of<ThemeProvider>(context, listen: false)
+                        .toggleTheme()),
+          ],
+        ),
+      ),
     );
   }
 }
